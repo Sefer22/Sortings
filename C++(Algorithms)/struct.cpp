42c298438d160e05ca72bf8d72eab9d72712d72a
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2023-12-10 14:17:16 +0400

    Add files via upload

--- C++(Algorithms)/struct.cpp
+++ C++(Algorithms)/struct.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+
+struct Student{
+	char name[15];
+	char surname[15];
+	int telNumber;
+	int age;
+	
+};
+
 int main() {
+	struct Student student = {"Safar","Jafarli",636,20};
+	
+	cout<<student.name<<student.surname<<student.telNumber<<student.age;
 	
 	
 	return 0;
